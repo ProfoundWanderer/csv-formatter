20988dd7347f3c2e501a4c54abefabd97a5a428b
Derrick <freemandc@vcu.edu>
2018-09-27 20:14:39 -0500

    just added link to github on website really

--- csvsite/cleancsv/views.py
+++ csvsite/cleancsv/views.py
@@ -7,6 +7,7 @@ from django.http import HttpResponse
 import time
 import pandas as pd
 
+
 # Create your views here.
 
 
@@ -29,16 +30,17 @@ def uploadcsv(request):
             return HttpResponseRedirect(reverse("cleancsv:upload_csv"))
 
         """
+        https://stackoverflow.com/questions/32787838/how-to-pass-data-between-django-views
         Current Issues:
-        3. If not first_name and last_name then find name to split it - if full name and not fist and last then
-          - Look for FirstName or something
-        3. if not phone look for mobile_phone or phone_number or something
-        3. if not email look for email address or something
-        2. makes sec contact email and phone sometimes when its not needed
-        1. Ian in try_combine_rows disappears!? find out why
+        2. If not first_name and last_name then find name to split it - if full name and not fist and last then
+            - Look for FirstName or something
+        2. if not phone look for mobile_phone or phone_number or something
+        2. if not email look for email address or something
+        1. makes sec contact email and phone sometimes when its not needed
         """
+
+        start_time = time.time()
         # point to file location.
-        start = time.time()
         df = pd.read_csv(csv_file)
         df.columns = [i.lower().replace(' ', '_') for i in df.columns]  # lower case and replace spaces
         df.index += 2  # so when it says "check these lines" the numbers match with csv
@@ -53,7 +55,8 @@ def uploadcsv(request):
         # Prints the lines where phone len is less than 8 or greater than 15. May not be needed now.
         phone_list = df['phone']
         phone_list = phone_list.replace('[^0-9]+', '', regex=True)  # remove special characters
-        phone_len = phone_list.astype(str).str.len()  # had to add .astype(str) before .str in order for certain csvs to work
+        phone_len = phone_list.astype(
+            str).str.len()  # had to add .astype(str) before .str in order for certain csvs to work
         phone_bad = list(phone_len[(phone_len > 0) & (phone_len < 8) | (phone_len > 15)].index)
         # print(f'Check these lines for an incomplete phone number: {phone_bad}')
 
@@ -62,7 +65,8 @@ def uploadcsv(request):
         # not sure if this is the best way
         # keeping name column for now to be safe.
         if 'first_name' and 'last_name' not in cols:
-            df[['first_name', 'last_name']] = df.name.str.split(' ', 1, expand=True)
+            if 'name' in cols:
+                df[['first_name', 'last_name']] = df.name.str.split(' ', 1, expand=True)
 
         """
         # checks to see if they have a name column or first and last name cols
@@ -109,7 +113,6 @@ def uploadcsv(request):
         df['second_contact_phone'] = df[~df['phone'].astype(str).str.contains(pat=r'^(?:(?!^.{,7}$|^.{16,}$).)*$', case=False, na=False)]['phone']
         df['phone'] = df[df['phone'].astype(str).str.contains(pat=r'^(?:(?!^.{,7}$|^.{16,}$).)*$', case=False, na=False)]['phone']
 
-
         # needed for below merger. Gets column headers
         get_col_headers = str(list(df))
         # gets rid of [''] around header names
@@ -137,14 +140,12 @@ def uploadcsv(request):
                 row.first_dupe = False
             return row
 
-
         df = df.apply(combine_rows, axis=1, result_type=None)
         # drops dup emails but keep first instance since everything should have been merged into that but ignores cells that are empty because
         # before it would just delete all rows with an empty email cell but the first one.....
-        df = df[df['email'].isnull() | ~df[df['email'].notnull()].duplicated(subset='email',keep='first')]
+        df = df[df['email'].isnull() | ~df[df['email'].notnull()].duplicated(subset='email', keep='first')]
         df.groupby('email').agg(lambda x: ", ".join(x)).reset_index()
         del df['first_dupe']
-
 
         # gets rid of random nan that pops up sometimes
         df = df.replace('(?:^|\W)nan(?:$|\W)', '', regex=True)
@@ -152,12 +153,13 @@ def uploadcsv(request):
         # Convert names back from ex. first_name so system auto catches it
         df.columns = [i.title().replace('_', ' ') for i in df.columns]
         done = df.to_csv(index=False)
-        end = time.time()
-        runtime = end - start
+
+        end_time = time.time()
+        runtime = end_time - start_time
+        print(runtime)
 
         response = HttpResponse(done, content_type='text/csv')
         response['Content-Disposition'] = 'attachment; filename="done.csv"'
-        speed = render(request, 'cleancsv/upload_csv.html', {"runtime": runtime})
         return response
 
     except Exception as e:
