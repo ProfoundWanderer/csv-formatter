d066b330c40e297bb84761fdfa16eb3a6642325c
Derrick3 <derrick@Derricks-MacBook-Pro.local>
2018-10-25 14:12:13 -0500

    Removed sep and engine since it gave python engine cannot iterate and delimiter has not been much of an issue but will look into it later down the line

--- csvsite/cleancsv/views.py
+++ csvsite/cleancsv/views.py
@@ -32,10 +32,13 @@ def uploadcsv(request):
             """
 
         start_time = time.time()
-        # point to file location.
-        # dtype=str so columns don't sometimes have .0 added and encoding to solve UnicodeDecodeError
-        # removed sep=None (where pandas tries to get the delimiter) but raises sep=None so pandas tries to get the delimiter and 
-        df = pd.read_csv(csv_file, sep=None, dtype=str, engine='python', encoding='ISO-8859-1')
+        """
+        - Pandas read csv file and assigns it to df.
+        - dtype=str so columns don't sometimes have .0 added and encoding to solve UnicodeDecodeError
+        - Removed sep=None (where pandas tries to get the delimiter) but raises sep=None so pandas tries to get the delimiter
+            if delimiters become an issue then I can try regex instead of None like I was trying to use before
+        """
+        df = pd.read_csv(csv_file, dtype=str, encoding='ISO-8859-1')
         df.columns = [i.lower().replace(' ', '_') for i in df.columns]  # lower case and replace spaces
         df.index += 2  # so when it says "check these lines" the numbers match with csv
         # removes empty rows then empty columns
